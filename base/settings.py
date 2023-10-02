0963d48b50a691f86c19621b30c892759b98789b
Jhulyan Mesa <jhlyan64@hotmail.com>
2023-10-02 10:42:17 -0500

    .

--- base/settings.py
+++ base/settings.py
@@ -150,7 +150,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'felminasogamoso@gmail.com'  # Tu dirección de correo electrónico
-EMAIL_HOST_PASSWORD = 'lbxavigxjepopmjy'  # Tu contraseña de correo electrónico
+EMAIL_HOST_PASSWORD = 'vakudacbtlqcybwg'  # Tu contraseña de correo electrónico
 EMAIL_USE_TLS = True
 # settings.py
 
